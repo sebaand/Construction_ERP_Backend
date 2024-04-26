dabd41a61881093d8b0d63d7e3969e11485ed1e8
sebaand <andreasebastio014@gmail.com>
2024-04-25 22:13:28 -0500

    fixed small bug in Mongo_URL declaration

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from app.api import app
 
 
 if __name__ == "__main__":
-    uvicorn.run("app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
+# import uvicorn
+
+# if __name__ == "__main__":
+#     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
